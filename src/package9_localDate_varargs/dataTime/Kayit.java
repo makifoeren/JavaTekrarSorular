daf867781a2ec910c151e2259e8060dfbfbdce18
makifoeren <makif.oeren@gmail.com>
2022-04-20 20:17:12 +0200

    20.04

--- src/package9_localDate_varargs/dataTime/Kayit.java
+++ src/package9_localDate_varargs/dataTime/Kayit.java
@@ -18,21 +18,27 @@ public class Kayit {
 
         Kullanici k1 = new Kullanici(isim, LocalDateTime.now()); // kullanici class ta pli con ile obj creat ettik
 
-        kisiler.add(k1);
+        kisiler.add(k1); // kullanici classdan p li cons ile create edilen obj kisler listesine eklendi
 
         return kisiler;
     }
 
-    public void sansliKullanici(List<Kullanici>kll){
+    public void sansliKullanici(List<Kullanici>kll){ // kayirAl() methodunda creat edilen her bir kullanici döngüye sokulda
 
-        for (Kullanici k:kll
+        for (Kullanici k:kll //
              ) {
             if (k.kayitZamani.getSecond()<10){
+                System.out.println("Sansli kisisiniz " + k.name + " süpriz hediye kazandiniz...");
 
-            }
+            }else System.out.println("Sansli kisi degilsiniz " + k.name + " bir dahaki sefere...");
 
         }
 
+    }
+    public void lsitele(List<Kullanici>kullanicilar){
+        for (Kullanici k :kullanicilar) { // kayitAl() methoduyla gelen kisiler lisitini döngüye sokuyor
+            System.out.println(" Adi : " + k.name + " kayit zamani : " + k.kayitZamani); //  kisiler listetdeki her bir kullanicinin ad ve kayit zamani
+        }
     }
 
 }
--- src/package9_localDate_varargs/dataTime/Main.java
+++ src/package9_localDate_varargs/dataTime/Main.java
@@ -1,12 +1,55 @@
 package package9_localDate_varargs.dataTime;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
 
+        Kayit yeniKayit = new Kayit(); // obj ile method coll yapilacak
+
+        List<Kullanici> kisi = new ArrayList<>();
+
+        boolean cikilsinMi = false;
+
+        while (!cikilsinMi) {
+            System.out.println("Lutfen \n1: Kayit al \n2 : Sansli kisi bul \n3: kisileri listele \n4: cikis ");
+            System.out.println("Islem seciniz...");
+           int islem = scan.nextInt();
+
+            switch (islem) {
+
+                case 1:
+                    kisi = yeniKayit.kayitAll(); // kisi listi degerini yeniKayit obj ile kayital method coll dan
+
+                    break;
+
+                case 2:
+                    yeniKayit.sansliKullanici(kisi);
+
+                    break;
+
+                case 3:
+                    yeniKayit.lsitele(kisi);
+
+                    break;
+
+                case 4:
+                    cikilsinMi = false;
+
+                    break;
+
+                default:
+                    System.out.println("Hatali veri girdiniz...");
 
 
+            }
+
+        }
     }
 }
