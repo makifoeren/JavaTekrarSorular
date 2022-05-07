36f09fdce1ada783a148c3c2dfe4ba12f743ad20
makifoeren <makif.oeren@gmail.com>
2022-05-08 00:43:52 +0200

    08.05

---
+++ src/interview_2/Q01_ArmstrongNumber.java
@@ -0,0 +1,57 @@
+package interview_2;
+
+import java.util.Scanner;
+
+public class Q01_ArmstrongNumber {
+    /*  soru1: Check if the integer is an Armstrong numbers to 4 digit numbers(4 digit-> exclusive)
+        Armstrong sayi:herhangi bir 3 basamakli sayinin rakamlarinin kupleri toplami o sayiyi veriyorsa sayi  Armstrong sayidir
+            (0, 1, 153, 370, 371, 407)
+            153 = 1*1*1 + 5*5*5 + 3*3*3 = 153
+            370 = 3*3*3 + 7*7*7 + 0*0*0 = 370
+ 370 = 3*3*3 + 7*7*7 + 0*0*0 = 370
+
+
+          soru2: Birden baslayarak girilen sayıya kadar her bir tamsayının amstrong sayı olup olmadıgını
+          gösteren program yazınız */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayi giriniz = ");
+        int sayi = scan.nextInt();
+
+        armstrong(sayi);
+        girilenSayiyaKadarOlanAmstrong(sayi);
+    }
+
+
+
+    private static void armstrong(int n) {
+        int sayininKupleriToplami = 0;
+        int basamaktakiSayi = 0;
+        int girilenSayi = n;
+
+        while (n > 0) {
+            basamaktakiSayi = n % 10;
+            sayininKupleriToplami = sayininKupleriToplami + (basamaktakiSayi * basamaktakiSayi * basamaktakiSayi);
+            n = n / 10;
+        }
+
+        if (girilenSayi == sayininKupleriToplami)
+            System.out.println(girilenSayi + " sayisi amstrong sayidir.");
+        else System.out.println(girilenSayi + " sayisi amstrong sayi degildir");
+
+        System.out.println("********************************************");
+
+    }
+    // soru 2 icin
+
+    private static void girilenSayiyaKadarOlanAmstrong(int sayi) {
+        for (int i = 1; i <=sayi ; i++) {
+            armstrong(i); //recursive method uyguladik
+
+        }
+
+
+
+    }
+}
---
+++ src/interview_2/Q05_DifferenceBetweenArray_LargestAndSmallestElements.java
@@ -0,0 +1,49 @@
+package interview_2;
+
+import java.util.*;
+
+public class Q05_DifferenceBetweenArray_LargestAndSmallestElements {
+
+      /*
+ Create a function that takes an array and the difference between the largest
+ and the smallest numbers.
+ Ask user to enter array elements.
+    kullanıcının girdigi bir array'in en buyuk elemani ile
+    en kucuk elemanının  farkını bulan bir method create ediniz.
+ */
+
+    public static void main(String[] args) {
+        List<Integer> sayi = new ArrayList<>(Arrays.asList(4, 2, 6, 11));
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Array uzunlugunuzu giriniz...");
+        int uzunluk = scan.nextInt();
+        int arr[] = new int[uzunluk];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println("Arrayin " + (i + 1) + ".elemanini giriniz");
+            arr[i] = scan.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Array in son elemni ile ilk elemanin farki : " + (arr[arr.length - 1] - arr[0]));
+
+        System.out.println("**************************");
+
+        arrMethod(sayi);
+
+    }
+
+    private static void arrMethod(List<Integer> sayi) {
+
+        Optional<Integer> max = sayi.stream().reduce(Integer::max);
+
+        Optional<Integer> min = sayi.stream().reduce(Integer::min);
+
+        System.out.println("Lambda ile yapilan :" + (Collections.max(sayi) - Collections.min(sayi)));
+
+
+    }
+
+}
